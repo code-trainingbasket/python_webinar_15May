f5533d3f93c47be956a40487a0c611dfd4c55c93
TrainingBasket <sourcecode@trainingbasket.in>
2020-05-15 18:15:11 +0530

    Added api related code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import flask
 
-from flask import render_template
+from flask import render_template,request,jsonify
 
 app = flask.Flask(__name__)
 
@@ -13,6 +13,25 @@ def home():
 @app.route('/blogs',methods=['GET'])
 def blogs():
 	return render_template('blogs.html')
+
+@app.route('/api/v1/checkprime', methods=['GET'])
+def check_prime():
+
+	result = {}
+	if 'num' in request.args:
+		n = int(request.args['num'])
+	else:
+		result = {'cod':400,'meesage':"No 'num' provided"}
+		return jsonify(result)
+
+	for i in range(2,n//2+1):
+		if n*i == 0:
+			prime = False
+			break
+	else:
+		prime = True
+	result = {'Number':n,'IsPrime':prime}
+	return jsonify(result)
 
 if __name__ == "__main__":
 	app.run(port =8000)
